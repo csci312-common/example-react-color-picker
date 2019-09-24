5988fc3dd3f9e8ffebee93a568b3931fdf24738f
Christopher Andrews <candrews@middlebury.edu>
2019-09-23 23:32:00 -0400

    Bubble up state

--- src/App.js
+++ src/App.js
@@ -2,30 +2,32 @@ import React, { useState } from 'react';
 import './App.css';
 
 
-function LabeledSlider({ label }){
-  const [value, setValue] = useState(0);
-
-  return (
-    <div>
-        <div className="color-label">{label}:</div>
-        <input type="range" 
-        min="0" 
-        max="255"
-        value={value}
-        onChange={(event)=>{setValue(event.target.value)}} 
-        />
-        <span>{value}</span>
+function LabeledSlider({ label, value, setValue }) {
+  return (
+    <div>
+      <div className="color-label">{label}:</div>
+      <input type="range" 
+         min="0"
+         max="255"
+         value={value}
+         onChange={(event)=>{setValue(event.target.value)}}/>
+      <span>{value}</span>
       </div>
-  );
+  );
 }
 
 function ColorPicker() {
+  const [red, setRed] = useState(0);
+  const [green, setGreen] = useState(0);
+  const [blue, setBlue] = useState(0);
+
+  const color = {background: `rgb(${red}, ${green}, ${blue})`};
   return (
     <div className="color-picker">
-    <div className="color-swatch"></div>
-    <LabeledSlider label="red"/>
-    <LabeledSlider label="green"/>
-    <LabeledSlider label="blue"/>
+      <div className="color-swatch" style={color}></div>
+      <LabeledSlider label="red"   value={red}   setValue={(value)=>{setRed(value)}}/>
+      <LabeledSlider label="green" value={green} setValue={(value)=>{setGreen(value)}}/>
+      <LabeledSlider label="blue"  value={blue}  setValue={(value)=>{setBlue(value)}}/>
     </div>
   );
 }
